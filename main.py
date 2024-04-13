5432713330ffe19fa1916932dd35dacb7f33aaa3
lcz <bbccoolly@gmail.com>
2024-04-13 13:52:18 +0800

    加载模型、对其进行训练、评估其在验证集上的性能，甚至将其导出为ONNX 格式

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ results = model.train(data='coco128.yaml', epochs=1)
 results = model.val()
 
 # Perform object detection on an image using the model
-results = model('t1.JPG')
+results = model('https://ultralytics.com/images/zidane.jpg')
 
 # Export the model to ONNX format
-success = model.export(format='onnx')
+success = model.export(format='onnx')  # 这里省略了path参数
+# if success:
+#     import shutil
+#     shutil.move('path_to_your_onnx_file/bus_test.onnx', 'D:/workplace-python/yolo8Lcz/yolo8Lcz/modelLcz/bus_test.onnx')
+
